17e36ea012b8d11a127644ca02a1c7cde227c7a0
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-04-30 11:44:18 +0800

    placeholder untuk input tema tulisan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -246,7 +246,7 @@
                             id="message"
                             bind:value={message}
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Tema Tulisan...."
+                            placeholder="Topik Teknologi, Kesehatan, Pendidikan, dll"
                             required
                         />
                     </div>
